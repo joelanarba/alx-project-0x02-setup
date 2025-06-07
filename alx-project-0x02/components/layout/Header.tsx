ba081be7df6b502b10a30f24757913e3aa58f940
Joel Anarba <157653692+joelanarba@users.noreply.github.com>
2025-06-07 10:53:11 +0000

    fix: add missing /posts navigation link to Header component

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -16,6 +16,11 @@ const Header = () => {
               About
             </Link>
           </li>
+          <li>
+            <Link href="/posts" className="hover:underline">
+              Posts
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
