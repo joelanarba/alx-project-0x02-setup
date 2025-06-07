f48ae848204f3421f495317feb619b93f5f54448
Joel Anarba <157653692+joelanarba@users.noreply.github.com>
2025-06-07 09:42:57 +0000

    feat: add basic routing with /home and /about pages and navigation links in Header

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">This is the About Page</h1>
+    </main>
+  )
+}
---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">This is the Home Page</h1>
+    </main>
+  )
+}
